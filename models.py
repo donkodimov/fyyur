d21872c6e2de5f9caf119d77110539c207a66150
dwebsites <dondim@protonmail.com>
2022-02-24 18:34:26 +0100

    fix: Add app.config

--- models.py
+++ models.py
@@ -18,6 +18,7 @@ setup_db(app)
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SECRET_KEY"] = '79537d00f4834892986f09a100aa1edf'
     db.app = app
     db.init_app(app)
     migrate.init_app(app, db)
